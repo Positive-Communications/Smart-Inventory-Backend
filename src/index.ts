c6cd24020b00be2c9a256d84565fa5cf2562097d
dreamnerd <bryodiiidah@gmail.com>
2021-02-10 13:02:17 +0300

    updates

--- src/index.ts
+++ src/index.ts
@@ -77,7 +77,6 @@ app.get('/', ((req, res) => {
 
 app.post('/save-company/', (req, res) => {
     saveCompany(req.body).then(data => {
-        console.log(data);
         res.json({
             company: data
         });
